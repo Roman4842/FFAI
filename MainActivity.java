57f63ea3151b6a1dd46bbb013037be116c150070
Roman4842 <Sirotenkoroma30@gmail.com>
2026-02-14 13:58:35 +0100

    Update MainActivity.java with complete fixed code for FFAI project

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+// MainActivity.java
+
+package com.example.ffai;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Initialize components and set up any necessary data here.
+    }
+}
